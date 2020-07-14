c1b6fc3b2f39a622839c3fe885041120b9a52c2f
Richard Li <richardli125@gmail.com>
2020-07-14 06:41:22 -0400

    added conditional for navmenu

--- src/components/NavMenu/NavMenu.js
+++ src/components/NavMenu/NavMenu.js
@@ -10,34 +10,36 @@ const NavMenu = () => {
     <CartConsumer>
       {({ cart }) => (
         <div className={active ? 'navigation active' : 'navigation'}>
-        <div className='ham-btn' onClick={() => setActive(!active)}>
-          <span></span>
-          <span></span>
-          <span></span>
+          <div className='ham-btn' onClick={() => setActive(!active)}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          {active ? (
+            <div className='links'>
+              <div className='link'>
+                <Link to='/' onClick={() => setActive(!active)}>
+                  Home
+                </Link>
+              </div>
+              <div className='link'>
+                <Link to='/shop' onClick={() => setActive(!active)}>
+                  Shop
+                </Link>
+              </div>
+              <div className='link'>
+                <Link to='/about' onClick={() => setActive(!active)}>
+                  About
+                </Link>
+              </div>
+              <div className='link'>
+                <Link to='/cart' onClick={() => setActive(!active)}>
+                  {`Cart: ${cart.length}`}
+                </Link>
+              </div>
+            </div>
+          ) : null}
         </div>
-        <div className='links'>
-          <div className='link'>
-            <Link to='/' onClick={() => setActive(!active)}>
-              Home
-            </Link>
-          </div>
-          <div className='link'>
-            <Link to='/shop' onClick={() => setActive(!active)}>
-              Shop
-            </Link>
-          </div>
-          <div className='link'>
-            <Link to='/about' onClick={() => setActive(!active)}>
-              About
-            </Link>
-          </div>
-          <div className='link'>
-            <Link to='/cart' onClick={() => setActive(!active)}>
-              {`Cart: ${cart.length}`}
-            </Link>
-          </div>
-        </div>
-      </div>
       )}
     </CartConsumer>
   );
