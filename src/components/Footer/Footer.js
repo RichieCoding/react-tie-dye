6aae3cb0184d3dcbdfdbfdf78f245993452953fb
Richard Li <richardli125@gmail.com>
2020-08-06 02:59:25 -0400

    added my portfolio link on footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,27 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { AiOutlineInstagram } from 'react-icons/ai'
+import { AiOutlineInstagram } from 'react-icons/ai';
 import './footer.styles.scss';
 
 const Footer = () => {
   return (
     <div className='footer-container'>
-      <div className="logo">
+      <div className='logo'>
         <h3>Artsy Crafty Jassy&copy;</h3>
       </div>
-      <div className="social-icons">
-        <a id='insta-icon' href="https://www.instagram.com/shopacj/" target="_blank" rel="noopener noreferrer">
-          <AiOutlineInstagram size={25}/>
+      <div className='social-icons'>
+        <a
+          id='insta-icon'
+          href='https://www.instagram.com/shopacj/'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <AiOutlineInstagram size={25} />
         </a>
       </div>
-      <div className="terms-links">
-        <Link to="/privacy-policy">Privacy Policy</Link>
-        <Link to="/terms-and-conditions">Terms & Conditions</Link>
-        <Link to="/terms-and-conditions">Affliations</Link>
+      <div className='terms-links'>
+        <Link to='/privacy-policy'>Privacy Policy</Link>
+        <Link to='/terms-and-conditions'>Terms & Conditions</Link>
+        <a href="http://richiecoding.com" target="_blank" rel="noopener noreferrer">RichieCoding</a>
       </div>
-      <div className="footer"></div>
+      <div className='footer'></div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
